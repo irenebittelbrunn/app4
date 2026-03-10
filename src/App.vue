fc777852d83bcf0dba0a8e1cc4dcf07785d5715f
irenebittelbrunn <irenebittelbrunnp@gmail.com>
2026-03-10 12:00:12 -0300

    odnwefds

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue'
+
 let tarefas = ref([
   'Prova de matemática',
   'Prova de geografia',
@@ -9,55 +10,58 @@ let tarefas = ref([
   'Seminario geografia',
   'Prova de fisica'
 ])
+
 let item = ref("")
+
 function adicionar() {
-  if (add.value.trim()){
-    tarefas.value.push(add.value);
-    aviso.value = false
+  if (item.value.trim()) {
+    tarefas.value.push(item.value)
+    item.value = ""
   }
-  else{
-    if(add.value.trim()){
-         tarefas.value.push(add.value);
-         aviso.value.splice(index, 1)
-    }
-  }
-  }
+}
 
-
+function deleteTarefa(index) {
+  tarefas.value.splice(index, 1)
+}
 </script>
+
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
+
     <input type="text" v-model="item"> <br>
     <button @click="adicionar">Confirmar</button>
-<ul>
-  <li v-for="tarefa in tarefas" :key="tarefa">
-    {{ tarefa }}</li>
-</ul>
+
+    <ul>
+      <li v-for="(tarefa, index) in tarefas" :key="index">
+        <a href="#" @click.prevent="deleteTarefa(index)">Delete</a>
+        {{ tarefa }}
+      </li>
+    </ul>
   </div>
 </template>
-
 <style scoped>
-input{
+input {
   background-color: brown;
   border: none;
   font-size: 2rem;
   border-radius: 50px;
   color: white;
 }
-button{
+
+button {
   background-color: brown;
   border: none;
   border-radius: 50px;
   font-size: 1.5rem;
   margin: 1vw;
-  
-}
-h1{
-  font-size: 3rem;
-}
-ul{
-  font-size: 1.5rem;
 }
 
+h1 {
+  font-size: 3rem;
+}
+
+ul {
+  font-size: 1.5rem;
+}
 </style>
