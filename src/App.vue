b581304bb55b0123564a58601702896a043c1943
irenebittelbrunn <irenebittelbrunnp@gmail.com>
2026-03-17 11:34:12 -0300

    acabou

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,64 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref } from 'vue';
 let tarefas = ref([
-  'Prova de matemática',
-  'Prova de geografia',
-  'Trabalho sociologia',
-  'Seminário portugues',
-  'Seminário educacão fisica',
-  'Seminario geografia',
-  'Prova de fisica'
+  'Prova Matemática',
+  'Prova Geografia',
+  'Trabalho Sociologia',
+  'Seminário Português',
+  'Seminário Educação Física',
+  'Seminário Geografia',
+  'Prova Física'
 ])
-
-let item = ref("")
-
+let aviso = ref(false);
+let add = ref("")
 function adicionar() {
-  if (item.value.trim()) {
-    tarefas.value.push(item.value)
-    item.value = ""
+  if (add.value.length < 5) {
+  } else {
+    if (add.value.trim()) {
+      tarefas.value.push(add.value);
+      aviso.value = false
+      add.value = ""
+    }
   }
 }
 
-function deleteTarefa(index) {
-  tarefas.value.splice(index, 1)
+function editarTarefa(item) {
+  const pos = tarefas.value.indexOf(item);
+  const edicao = prompt(`Editando ${item}...`);
+  if (edicao.length < 5) {
+    alert("Insira no mínimo 5 caracteres")
+  } else {
+    tarefas.value.splice(pos, 1, edicao);
+  }
+
+
 }
+
+function deleteTarefa(item) {
+  const pos = tarefas.value.indexOf(item);
+  tarefas.value.splice(pos, 1);
+}
+
+
 </script>
 
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
-
-    <input type="text" v-model="item"> <br>
-    <button @click="adicionar">Confirmar</button>
-
+    <input type="text" v-model="add" @keyup.enter="adicionar">
+    <button @click="adicionar">CONFIRMAR</button>
+    <p v-show="aviso">Insira no mínimo 5 caracteres</p>
     <ul>
-      <li v-for="(tarefa, index) in tarefas" :key="index">
-        <a href="#" @click.prevent="deleteTarefa(index)">Delete</a>
+      <li v-for="tarefa in tarefas" :key="tarefa">
+        <a href="#" @click.prevent="editarTarefa(tarefa)">E</a>
+        <a href="#" @click.prevent="deleteTarefa(tarefa)">D</a>
         {{ tarefa }}
       </li>
     </ul>
+    <button @click="tarefas.sort()">Ordenar</button>
   </div>
 </template>
+
 <style scoped>
 input {
   background-color: brown;
